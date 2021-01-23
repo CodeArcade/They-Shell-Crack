fed3c353723ec1d91a623c6a8c54f7ff65b464b1
Cyklan <37187056+Cyklan@users.noreply.github.com>
2021-01-23 11:23:01 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LessRoomyMoreShooty
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
